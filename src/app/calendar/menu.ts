a739f38ca2b214c2c7197872db96f1b7f24fd166
sonkah <annabaran512@gmail.com>
2018-08-11 18:15:10 +0200

    Added Navigation Bar

--- src/app/calendar/menu.ts
+++ src/app/calendar/menu.ts
@@ -1,10 +1,16 @@
 import { NbMenuItem } from '@nebular/theme';
 
-export const MENU: NbMenuItem[] = [
+export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: "Dashboard",
     icon: 'nb-home',
     link: '/calendar/dashboard',
     home: true,
+  },
+  {
+    title: "Log Out",
+    icon: 'nb-auth',
+    link: '/pages',
+    home: false,
   }
 ];
